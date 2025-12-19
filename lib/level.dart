224114033bc9abb6b8dd8d3b4c3b5fc2b9d7c256
Marcell07x <bmarci891@gmail.com>
2025-12-19 22:41:54 +0100

    worked on setLevel(), not finished

--- lib/level.dart
+++ lib/level.dart
@@ -2,10 +2,32 @@ import "package:shared_preferences/shared_preferences.dart";
 
 class WorkoutLevel {
     late int level;
-
+    late int resultLevel;
+    late int roundedResultLevel;
+    late int incSpeed;
+    late int previousStep;
+    late int newStep;
+    
     Future<void> getLevel() async {
         final prefs = await SharedPreferences.getInstance();
         level = prefs.getInt('level') ?? 1;
+    }
+
+    Future<void> setLevel() async {
+        final prefs = await SharedPreferences.getInstance();
+
+        // incSpeed has not been set
+
+        resultLevel = level + incSpeed;
+
+        previousStep = level ~/ 5;
+        newStep = resultLevel ~/ 5;
+
+        if (newStep > previousStep) {
+            roundedResultLevel = newStep * 5;
+        }
+
+        await prefs.setInt('level', roundedResultLevel);
     }
 
 }
--- lib/workouta_reps.dart
+++ lib/workouta_reps.dart
@@ -3,14 +3,19 @@ import 'level.dart';
 
 class WorkoutAReps extends WorkoutA {
     var Reps = [];
+    late int levelR;
 
     Future<void> SetReps(List<Map<String, String>> workoutParts) async {
         WorkoutLevel workoutLevel = WorkoutLevel();
-        await workoutLevel.getLevel();  
+        await workoutLevel.getLevel(); 
 
-        if (workoutLevel.level == 1) {
+        workoutlevel.level = levelR; 
+
+        if (levelR == 1) {
             Reps.add('10');
         }
+
+        if (levelR == 5 || ) // Problem: if the level increment skips a difficulty increase, that increase won't happen
 
         for (int i = 0; i < workoutParts.length; i++) {
             workoutParts[i]['reps'] = Reps[i];
